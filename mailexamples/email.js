74f6141d15a330ae7e2caa8d6fd5e65664b19edc
pooja143-tech <tundurthypoojitha070@gmail,com>
2021-01-22 14:18:38 +0530

    mail

---
+++ mailexamples/email.js
@@ -0,0 +1,23 @@
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'poojithakajal202@gmail.com',
+    pass: 'kaju143p'
+  }
+});
+
+var mailOptions = {
+  from: 'poojithakajal020gmail.com',
+  to: 'tundurthypoojitha070@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
